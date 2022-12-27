5305d23f470f95d755c45e9c9add06687bf92231
J. Géza <gezajoni87@gmail.com>
2022-12-27 10:43:02 +0100

    Update server.js

--- SOP_vizsga/server/core/server.js
+++ SOP_vizsga/server/core/server.js
@@ -24,35 +24,7 @@ class Server
         APP.use('/api', RouterClass.ROUTER);
         APP.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-        // APP.get('/b', (req, res) => { //Line 9
-        //     res.write({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
-        // });
-
-        //make a /hello route that returns a json object with a message property
-        APP.get('/hello', (req, res) => {
-            res.json({ message: 'Hello World!' });
-        });
-
-        // ROUTER = express.Router();
-        // export router
-
-
-        
-
-
     }
 }
-// const app = express();
-// const port = process.env.PORT || 5000;
-
-// This displays message that the server running and listening to specified port
-// app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
-
-// create a GET route
-// app.get('/express_backend', (req, res) => { //Line 9
-//   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
-// });
 
 export default Server;
-
-//export ROUTER
